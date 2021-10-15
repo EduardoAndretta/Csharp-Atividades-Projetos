5597547b4f25a17abc4b9816850b9e982788b294
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:05:11 -0300

    Manipulação - Saldo

---
+++ ManipulaçãoSaldo/Empresa2/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 22/06/2021
+ * Time: 21:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Empresa2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(34, 194);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(253, 26);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(34, 120);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(253, 26);
+			this.textBox2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(82, 273);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(142, 37);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Armazenar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(319, 23);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(317, 564);
+			this.listBox1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(319, 593);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(317, 37);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Exibir";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(34, 86);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(136, 31);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Nome";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(34, 160);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(153, 31);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Saldo Médio";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(651, 654);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Empresa2";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ ManipulaçãoSaldo/Empresa2/MainForm.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 22/06/2021
+ * Time: 21:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Empresa2
+{
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		
+		}
+		
+		string[] nomes = new string[50];
+		float[] saldos = new float[50];
+		float[] saldosBonus = new float[50];
+		float[] saldoTotal = new float[50];
+		
+		int cont = 0;
+		float saldoBonus = 0;
+		float saldo = 0;
+		string nome = "";
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+			if(cont == 49){
+                
+                button1.Enabled = false;
+                MessageBox.Show("Limite Alcançado");
+            
+            }
+
+			saldo = 0;
+			saldoBonus = 0;
+			nome = "";
+			
+			saldos[cont] = float.Parse(textBox1.Text);
+			saldo = float.Parse(textBox1.Text);
+			nomes[cont] = textBox2.Text;
+			
+			if(saldo >= 0 && saldo <= 200){
+			   	
+				saldoBonus = 0;
+				saldosBonus[cont] = saldoBonus;
+			
+			}else if(saldo >= 201 && saldo <= 400){
+				
+				saldoBonus = (float)(saldo * 0.2);
+				saldosBonus[cont] = saldoBonus;
+			
+			}else if(saldo >= 401 && saldo <= 600){
+				
+				saldoBonus = (float)(saldo * 0.3);	
+				saldosBonus[cont] = saldoBonus;
+			
+			}else if(saldo >= 601){
+				
+				saldoBonus = (float)(saldo * 0.4);	
+				saldosBonus[cont] = saldoBonus;
+			
+			}else{
+					
+				MessageBox.Show("Digite um número válido");
+				
+			}
+			
+	
+			saldoTotal[cont] = saldoBonus + saldo;
+			
+			
+			cont++;
+
+			textBox1.Clear();
+			textBox2.Clear();
+			
+			listBox1.Items.Clear();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+			listBox1.Items.Clear();
+			
+			for(int i = 0; i<cont; i++){
+				
+				listBox1.Items.Add("Nome:" +nomes[i]);
+				listBox1.Items.Add("Saldo:" +saldos[i]);
+				listBox1.Items.Add("Saldo Bônus:" +saldosBonus[i]);
+				listBox1.Items.Add("");
+				listBox1.Items.Add("Total:" +saldoTotal[i]);
+				listBox1.Items.Add("");
+				
+			}
+			
+			
+			
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
