74d3d2591eb3376b0de8a49702d15f6073ebac1f
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:09:23 -0300

    Organização - Notas

---
+++ SeparaçãoDeNotas/Empresa3/MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 22/06/2021
+ * Time: 22:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Empresa3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.ListBox listBox3;
+		private System.Windows.Forms.ListBox listBox4;
+		private System.Windows.Forms.ListBox listBox5;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.ListBox listBox6;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ListBox listBox7;
+		private System.Windows.Forms.Label label3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.listBox3 = new System.Windows.Forms.ListBox();
+			this.listBox4 = new System.Windows.Forms.ListBox();
+			this.listBox5 = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.listBox6 = new System.Windows.Forms.ListBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.listBox7 = new System.Windows.Forms.ListBox();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(98, 46);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(182, 32);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Valor do Saque";
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 29;
+			this.listBox1.Location = new System.Drawing.Point(39, 67);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(343, 62);
+			this.listBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(24, 180);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(343, 36);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Calcular";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(24, 236);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(343, 36);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Limpar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(24, 91);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(343, 26);
+			this.textBox1.TabIndex = 5;
+			// 
+			// listBox2
+			// 
+			this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 29;
+			this.listBox2.Location = new System.Drawing.Point(39, 186);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(343, 62);
+			this.listBox2.TabIndex = 6;
+			// 
+			// listBox3
+			// 
+			this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox3.FormattingEnabled = true;
+			this.listBox3.ItemHeight = 29;
+			this.listBox3.Location = new System.Drawing.Point(39, 290);
+			this.listBox3.Name = "listBox3";
+			this.listBox3.Size = new System.Drawing.Size(343, 62);
+			this.listBox3.TabIndex = 7;
+			// 
+			// listBox4
+			// 
+			this.listBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox4.FormattingEnabled = true;
+			this.listBox4.ItemHeight = 29;
+			this.listBox4.Location = new System.Drawing.Point(39, 402);
+			this.listBox4.Name = "listBox4";
+			this.listBox4.Size = new System.Drawing.Size(343, 62);
+			this.listBox4.TabIndex = 8;
+			// 
+			// listBox5
+			// 
+			this.listBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox5.FormattingEnabled = true;
+			this.listBox5.ItemHeight = 29;
+			this.listBox5.Location = new System.Drawing.Point(39, 516);
+			this.listBox5.Name = "listBox5";
+			this.listBox5.Size = new System.Drawing.Size(343, 62);
+			this.listBox5.TabIndex = 9;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(403, 147);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(217, 105);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 10;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(403, 37);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(217, 104);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 11;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(403, 258);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(217, 104);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 12;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(403, 479);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(217, 105);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox4.TabIndex = 13;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(403, 368);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(217, 105);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox5.TabIndex = 14;
+			this.pictureBox5.TabStop = false;
+			// 
+			// listBox6
+			// 
+			this.listBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox6.FormattingEnabled = true;
+			this.listBox6.ItemHeight = 29;
+			this.listBox6.Location = new System.Drawing.Point(24, 540);
+			this.listBox6.Name = "listBox6";
+			this.listBox6.Size = new System.Drawing.Size(343, 62);
+			this.listBox6.TabIndex = 15;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(129, 492);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(182, 32);
+			this.label2.TabIndex = 16;
+			this.label2.Text = "Exedente";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Menu;
+			this.panel1.Controls.Add(this.pictureBox5);
+			this.panel1.Controls.Add(this.pictureBox4);
+			this.panel1.Controls.Add(this.pictureBox3);
+			this.panel1.Controls.Add(this.pictureBox2);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.listBox5);
+			this.panel1.Controls.Add(this.listBox4);
+			this.panel1.Controls.Add(this.listBox3);
+			this.panel1.Controls.Add(this.listBox2);
+			this.panel1.Controls.Add(this.listBox1);
+			this.panel1.Location = new System.Drawing.Point(384, 24);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(637, 603);
+			this.panel1.TabIndex = 17;
+			// 
+			// listBox7
+			// 
+			this.listBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox7.FormattingEnabled = true;
+			this.listBox7.ItemHeight = 29;
+			this.listBox7.Location = new System.Drawing.Point(24, 364);
+			this.listBox7.Name = "listBox7";
+			this.listBox7.Size = new System.Drawing.Size(343, 62);
+			this.listBox7.TabIndex = 18;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(85, 329);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(240, 32);
+			this.label3.TabIndex = 19;
+			this.label3.Text = "Valor Requissitado";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Silver;
+			this.ClientSize = new System.Drawing.Size(1033, 639);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.listBox7);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.listBox6);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Empresa3";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ SeparaçãoDeNotas/Empresa3/MainForm.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 22/06/2021
+ * Time: 22:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Empresa3
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+		
+			InitializeComponent();
+			
+		}
+	
+		int numeroSaque = 0;
+		string resposta = "";
+		
+		int nota200 = 0;
+		int nota100 = 0;
+		int nota50 = 0;
+		int nota20 = 0;
+		int nota10 = 0;
+		int resto = 0;
+		
+		
+		
+		void Button1Click(object sender, EventArgs e){
+			
+		nota200 = 0;
+		nota100 = 0;
+		nota50 = 0;
+		nota20 = 0;
+		nota10 = 0;
+		resto = 0;
+			
+			
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();
+			listBox3.Items.Clear();
+			listBox4.Items.Clear();
+			listBox5.Items.Clear();
+			listBox6.Items.Clear();
+			listBox7.Items.Clear();
+			
+			numeroSaque = int.Parse(textBox1.Text);
+				
+			//listBox7.Items.Add("R$"+numeroSaque);
+			
+			//listBox1.Items.Add("Notas de R$200,00: " +numeroSaque/200);
+			
+			//numeroSaque =  numeroSaque - numeroSaque/200*200;
+			
+			//listBox2.Items.Add("Notas de R$100,00: " +numeroSaque/100);
+			
+			//numeroSaque =  numeroSaque - numeroSaque/100*100;
+			
+			//listBox3.Items.Add("Notas de R$50,00: " +numeroSaque/50);
+			
+			//numeroSaque = numeroSaque % 50;
+			
+			//listBox4.Items.Add("Notas de R$20,00: " +numeroSaque/20);
+			
+			//numeroSaque = numeroSaque % 20;
+			
+			//listBox5.Items.Add("Notas de R$10,00: " +numeroSaque/10);
+			
+			//numeroSaque = numeroSaque % 10;
+			
+			//listBox6.Items.Add("Restante: R$" +numeroSaque);
+			
+			//textBox1.Clear();
+			
+		
+		
+		//Segunda opção 
+		
+		
+		for(int i = 0; i<numeroSaque; i++){
+			
+			if(numeroSaque >= 200){
+				
+				nota200 = nota200 + 1;
+				numeroSaque = numeroSaque - 200;	
+			
+			}if(numeroSaque >= 100 && numeroSaque < 200){
+				 
+				nota100 = nota100 + 1;
+				numeroSaque = numeroSaque - 100;	
+					
+			}if(numeroSaque >= 50 && numeroSaque < 100){
+				
+				nota50 = nota50 + 1;
+				numeroSaque = numeroSaque - 50;	
+						
+			}if(numeroSaque >= 20 && numeroSaque < 50){
+				
+				nota20 = nota20 + 1;
+				numeroSaque = numeroSaque - 20;	
+				
+			}if(numeroSaque >= 10 && numeroSaque < 20){
+				
+				nota10 = nota10 + 1;
+				numeroSaque = numeroSaque - 10;	
+					
+			}if(numeroSaque < 10){
+				
+				resto = numeroSaque;
+	
+			}
+			
+		}
+		
+		
+		
+		listBox1.Items.Add("Notas de R$200,00: " +nota200);
+		listBox2.Items.Add("Notas de R$100,00: " +nota100);
+		listBox3.Items.Add("Notas de R$50,00: " +nota50);
+		listBox4.Items.Add("Notas de R$20,00: " +nota20);
+		listBox5.Items.Add("Notas de R$10,00: " +nota10);
+		listBox6.Items.Add("Restante: " +resto);
+		
+		}
+		
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+			textBox1.Clear();
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();
+			listBox3.Items.Clear();
+			listBox4.Items.Clear();
+			listBox5.Items.Clear();
+			listBox6.Items.Clear();
+			listBox7.Items.Clear();
+	
+			
+			
+		}
+		void Label3Click(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
