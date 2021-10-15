d9676fc927aa42992d809267c066ed08baec4871
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:09:45 -0300

    Soma - Números

---
+++ Soma(Números)/Exemplo1/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 10/05/2021
+ * Time: 21:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Exemplo1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(69, 209);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(159, 57);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Some";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(82, 54);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(137, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(69, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(159, 29);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "First Number:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(55, 100);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(195, 29);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Second Number:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(82, 132);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(137, 26);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(82, 346);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(150, 26);
+			this.textBox3.TabIndex = 6;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(108, 286);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(81, 29);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Result";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(303, 420);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "Program - Calculator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Soma(Números)/Exemplo1/MainForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 10/05/2021
+ * Time: 21:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Exemplo1
+{
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e) //Método - Clique no Botão
+		{
+	
+			// componente.Propriedade
+			
+			int n1 = int.Parse(textBox1.Text);
+			int n2 = int.Parse(textBox2.Text);
+			
+			int soma = n1 + n2;
+			
+			textBox3.Text = soma.ToString();
+	
+			this.BackColor = Color.AliceBlue;
+		
+		
+		}
+}
+
+	
+}
