ba524ce24add3e2ee6749533d8468d71c6640604
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:13:38 -0300

    Vetor - Array

---
+++ Vetor-Array/Vetor-Array/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 07/06/2021
+ * Time: 21:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Vetor_Array
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button4;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(383, 76);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(304, 424);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(383, 570);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(304, 69);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Revelar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(70, 116);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(193, 26);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(69, 166);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(194, 69);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Armazenar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(72, 76);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(193, 27);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Registro:";
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(72, 499);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(193, 69);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Alterar";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(72, 445);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(193, 26);
+			this.textBox2.TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(177, 352);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(86, 26);
+			this.textBox3.TabIndex = 7;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(72, 351);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(102, 27);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Registro:";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(72, 404);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(193, 27);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Novo Nome";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(72, 600);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(41, 39);
+			this.button4.TabIndex = 10;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(754, 684);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "MainForm";
+			this.Text = "Vetor-Array";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Vetor-Array/Vetor-Array/MainForm.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 07/06/2021
+ * Time: 21:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vetor_Array
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		
+		}
+		
+		int cont = 0;
+		
+		string [] nomes = new string[10];
+		
+		//string [] nomes = {"Maria","Pedro","Juca","Marcio","Eliana","Valmir","Julia","Camila","Rosa","João"};
+			
+				
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+		//	nomes[0] = "Maria";
+		//	nomes[1] = "Pedro";
+		//	nomes[2] = "Juca";
+		//	nomes[3] = "Marcio";
+			
+		listBox1.Items.Clear();
+		
+		for(int i = 0; i < cont; i++){
+			
+			listBox1.Items.Add(nomes[i]);
+			
+			
+		}
+			
+			
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+			nomes[cont] = textBox1.Text;
+			textBox1.Clear();
+			
+			cont++;
+			label1.Text = "Registro:" + cont;
+			
+			if(cont == 10){
+				
+				button2.Enabled = false;
+				MessageBox.Show("Limite Alcançado");
+			
+			}
+			
+			Button1Click(this, null);
+			
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+			
+			int pos = int.Parse(textBox3.Text);
+			pos = pos - 1;
+			
+			nomes[pos] = textBox2.Text;
+			
+			textBox2.Clear();
+			textBox3.Clear();
+			
+			MessageBox.Show("Alteração efetuada com sucesso");
+			
+			Button1Click(this, null);
+		
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+	
+			string s = "CAVALO";
+			
+			for( int i =0; i <6; i++)
+				
+				listBox1.Items.Add(s[i]);
+			
+			
+			
+		}
+		
+		
+	}
+}
