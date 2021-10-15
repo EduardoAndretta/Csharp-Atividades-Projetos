19b9118635b22324801ebd4ef9c2a4a42f3810e6
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:07:21 -0300

    Parâmetro

---
+++ Parâmetro/Parametro/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 26/05/2021
+ * Time: 20:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Parametro
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(70, 203);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(123, 26);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(93, 129);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Número:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(54, 275);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(151, 46);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Verificar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(21, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(228, 25);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Entre 40 e 80 / Acima de 200";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(275, 407);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Parametro";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Parâmetro/Parametro/MainForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 26/05/2021
+ * Time: 20:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Parametro
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+			
+			float num = float.Parse (textBox1.Text);
+			
+			if((num >= 30 && num <= 80) || (num > 200)){
+				
+				
+				MessageBox.Show("Número Válido");
+				
+					
+			}else{
+				
+				
+				MessageBox.Show("Número Inválido");
+				
+			
+			}
+			
+			
+		}
+	}
+}
