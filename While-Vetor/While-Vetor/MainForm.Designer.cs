64ef9a06882f164cbf34ebc1da79deabd095a2a9
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:14:02 -0300

    While - Vetor

---
+++ While-Vetor/While-Vetor/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 09/06/2021
+ * Time: 19:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace While_Vetor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(355, 70);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(332, 584);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(75, 187);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(226, 55);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(75, 141);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(226, 26);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(75, 412);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(226, 55);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "While";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(75, 507);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(226, 55);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Foreach";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(75, 102);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(226, 36);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Nome:";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(737, 70);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(512, 508);
+			this.richTextBox1.TabIndex = 6;
+			this.richTextBox1.Text = "";
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(737, 599);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(130, 55);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "Salvar";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(936, 599);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(130, 55);
+			this.button5.TabIndex = 8;
+			this.button5.Text = "Abrir";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.Location = new System.Drawing.Point(1119, 599);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(130, 55);
+			this.button6.TabIndex = 9;
+			this.button6.Text = "Limpar";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1283, 728);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "MainForm";
+			this.Text = "While-Vetor";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ While-Vetor/While-Vetor/MainForm.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 09/06/2021
+ * Time: 19:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace While_Vetor
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+		
+		}
+		
+		//Variáveis globais (Fora de qualquer Método, mas dentro da Classe)
+			
+		string[] nomes = new string[10];
+			
+		int cont=0;
+		                       
+		void Button1Click(object sender, EventArgs e)
+		{
+		
+			//Variáveis locais (Dentro do Método)
+			
+			if (textBox1.Text !=""){
+				
+				nomes[cont] = textBox1.Text;
+				textBox1.Clear();
+				textBox1.Focus();
+				MessageBox.Show("Registro cadastrado com sucesso.");
+				
+			}else{
+				
+				MessageBox.Show("Preencha o campo nome.");
+				textBox1.Focus();
+			}
+			
+			
+				cont++;
+			
+			if(cont==10){
+				
+				button1.Enabled = false;
+			}
+		
+			
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+			//While
+				
+			int i = 0;
+			listBox1.Items.Clear();
+				
+			
+			while(i<cont){
+					
+					
+				listBox1.Items.Add(nomes[i]);
+				i++;
+				
+			}
+			
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+			listBox1.Items.Clear();
+			
+			//Exemplo Floreach = Para cada
+			
+			foreach(string nome in nomes){
+				
+				listBox1.Items.Add(nome);
+				
+			}
+			
+			
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+			for(int i=0; i<10; i++){
+				
+				nomes[i] = "";
+				
+			}
+			
+			MessageBox.Show("Vetor inicializado");
+			
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+	
+			//Botão Salvar
+			
+			richTextBox1.SaveFile("Teste1.txt",RichTextBoxStreamType.PlainText);
+			MessageBox.Show("Registro salvo com sucesso");
+			
+		}
+		
+		void Button5Click(object sender, EventArgs e)
+		
+		{
+	
+			richTextBox1.LoadFile("Teste1.txt",RichTextBoxStreamType.PlainText);
+			
+		}
+		
+		
+	}
+}
