c78f917481c53c00e3ea07b0bc2aea0d7710cae6
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:03:00 -0300

    Exemplo - Recuperar Dados

---
+++ ExemploRecuperarDados/ExemploRecuperarDados/MainForm.Designer.cs
@@ -0,0 +1,262 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 30/06/2021
+ * Time: 19:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ExemploRecuperarDados
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox5;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(22, 565);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(477, 35);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(22, 617);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(226, 45);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Salvar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(22, 524);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(268, 28);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Nome do Arquivo";
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(273, 617);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(226, 45);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Abrir";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(22, 27);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(477, 485);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = "";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(561, 27);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(278, 28);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Nome do Funcionário";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(544, 68);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(278, 35);
+			this.textBox2.TabIndex = 7;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(544, 122);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(278, 45);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "Adicionar";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 29;
+			this.listBox1.Location = new System.Drawing.Point(845, 27);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(312, 468);
+			this.listBox1.TabIndex = 10;
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(845, 536);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(312, 45);
+			this.button4.TabIndex = 11;
+			this.button4.Text = "Listar";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(1195, 205);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(153, 45);
+			this.button5.TabIndex = 12;
+			this.button5.Text = "Listar";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(1374, 210);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(99, 35);
+			this.textBox3.TabIndex = 13;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(1195, 146);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(278, 35);
+			this.textBox4.TabIndex = 14;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(1195, 106);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(278, 28);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "Texto";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(814, 601);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(515, 60);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "Quantidade de Palavras:";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(814, 661);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(628, 35);
+			this.label5.TabIndex = 17;
+			this.label5.Text = "Quantidade de Palavra Especifica:";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(814, 699);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(278, 35);
+			this.textBox5.TabIndex = 18;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1524, 746);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "ExemploRecuperarDados";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ ExemploRecuperarDados/ExemploRecuperarDados/MainForm.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 30/06/2021
+ * Time: 19:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExemploRecuperarDados
+{
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		
+		int contador = 0;
+		string[] nomes;
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+			//Botão Salvar
+
+			richTextBox1.SaveFile(textBox1.Text, RichTextBoxStreamType.PlainText);
+			MessageBox.Show("Arquivo salvo com sucesso","AVISO!");
+			
+			
+			
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+			//Botão Abrir
+			
+			try{
+			
+				richTextBox1.LoadFile(textBox1.Text, RichTextBoxStreamType.PlainText);
+				nomes = richTextBox1.Text.Split(' ','\n');
+				
+			}catch (Exception ex){
+				
+				System.Media.SystemSounds.Beep.Play();
+				MessageBox.Show("Erro ao abrir o arquivo","AVISO!");
+				
+			}
+				
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+			//Adcionar
+			
+			richTextBox1.Text += textBox2.Text + "\n" ;
+			
+			nomes[contador] = textBox2.Text;
+			nomes = richTextBox1.Text.Split(' ','\n');
+			
+			textBox2.Clear();
+			textBox2.Focus();
+			
+			
+			contador++;
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+	
+			//Listar
+			
+			int contPalavra = 0;
+			
+			foreach(string nome in nomes){
+				
+				listBox1.Items.Add(nome);
+				if(nome == textBox5.Text){
+					
+					contPalavra++;
+					
+				}
+				
+				
+			}
+			
+			label4.Text = "Quantidade de Palavras: " +nomes.Length;
+			label5.Text = "Quantidade de Palavras Especificas: " +contPalavra;
+			
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+	
+		
+			string texto = textBox4.Text;
+			char caracter = textBox3.Text[0];
+		
+			string[] vetor = texto.Split(caracter);
+			
+			//Listar
+			
+			listBox1.Items.Clear();
+			foreach(string elemento in vetor)
+				listBox1.Items.Add(elemento);
+			
+			
+		}
+	}
+}
