e43c29b20121360a48761c40e8d4ea24edb0ee86
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:01:04 -0300

    Atividade - Dados II

---
+++ Dados II/Dados/MainForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 15/05/2021
+ * Time: 15:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Dados
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button10;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.button9);
+			this.panel1.Controls.Add(this.button8);
+			this.panel1.Controls.Add(this.button7);
+			this.panel1.Controls.Add(this.button6);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(28, 147);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(615, 102);
+			this.panel1.TabIndex = 0;
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.Cyan;
+			this.button9.Location = new System.Drawing.Point(544, 28);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(43, 39);
+			this.button9.TabIndex = 8;
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.Button9Click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Gray;
+			this.button8.Location = new System.Drawing.Point(483, 28);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(43, 39);
+			this.button8.TabIndex = 7;
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.Button8Click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.AliceBlue;
+			this.button7.Location = new System.Drawing.Point(419, 28);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(43, 39);
+			this.button7.TabIndex = 6;
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.Button7Click);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Pink;
+			this.button6.Location = new System.Drawing.Point(351, 28);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(43, 39);
+			this.button6.TabIndex = 5;
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Yellow;
+			this.button5.Location = new System.Drawing.Point(289, 28);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(43, 39);
+			this.button5.TabIndex = 4;
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Blue;
+			this.button4.Location = new System.Drawing.Point(223, 28);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(43, 39);
+			this.button4.TabIndex = 3;
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Red;
+			this.button3.Location = new System.Drawing.Point(155, 28);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(43, 39);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.ForestGreen;
+			this.button2.Location = new System.Drawing.Point(88, 28);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(43, 39);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.Window;
+			this.button1.Location = new System.Drawing.Point(23, 28);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(43, 39);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button10
+			// 
+			this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button10.Location = new System.Drawing.Point(47, 25);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(95, 93);
+			this.button10.TabIndex = 1;
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.Button10Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(672, 267);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Dados";
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Dados II/Dados/MainForm.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 15/05/2021
+ * Time: 15:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Dados
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		Random rnd = new Random();
+		
+		void Button10Click(object sender, EventArgs e)
+		{
+	
+			int n = rnd.Next(1,7);
+			button10.Text = n.ToString();
+		
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+
+			this.BackColor = Color.White;
+			button10.BackColor = Color.WhiteSmoke;
+			
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+			this.BackColor = Color.Green;
+			button10.BackColor = Color.WhiteSmoke;
+		
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.Red;
+			button10.BackColor = Color.WhiteSmoke;
+			
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+		
+			this.BackColor = Color.Blue;
+			button10.BackColor = Color.WhiteSmoke;
+		
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.Yellow;
+			button10.BackColor = Color.WhiteSmoke;
+		
+		}
+		void Button6Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.Pink;
+			button10.BackColor = Color.WhiteSmoke;
+			
+		}
+		void Button7Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.AliceBlue;
+			button10.BackColor = Color.WhiteSmoke;
+			
+		}
+		void Button8Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.Gray;
+			button10.BackColor = Color.WhiteSmoke;
+			
+		}
+		void Button9Click(object sender, EventArgs e)
+		{
+	
+			this.BackColor = Color.Cyan;
+			button10.BackColor = Color.WhiteSmoke;	
+			
+		}
+	}
+}
