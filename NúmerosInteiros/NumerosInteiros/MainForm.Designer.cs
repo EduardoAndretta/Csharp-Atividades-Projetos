a8e4905452e1c88deffd0f54afd0796d18a245c9
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:06:48 -0300

    Números - Inteiros

---
+++ NúmerosInteiros/NumerosInteiros/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 26/05/2021
+ * Time: 19:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace NumerosInteiros
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(38, 170);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(68, 26);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(221, 170);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(68, 26);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(401, 170);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(68, 26);
+			this.textBox3.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 127);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(141, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Primeiro Número";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(189, 127);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(141, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Segundo Número";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(378, 127);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(141, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Terceiro Número";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(212, 248);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 56);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Exibir em Ordem";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(38, 377);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(292, 26);
+			this.textBox4.TabIndex = 10;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(38, 433);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(292, 26);
+			this.textBox5.TabIndex = 11;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(38, 490);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(292, 26);
+			this.textBox6.TabIndex = 12;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(515, 592);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "NumerosInteiros";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ NúmerosInteiros/NumerosInteiros/MainForm.cs
@@ -0,0 +1,218 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 26/05/2021
+ * Time: 19:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NumerosInteiros
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+			int a = int.Parse(textBox1.Text);
+			int b = int.Parse(textBox2.Text);
+			int c = int.Parse(textBox3.Text);
+			
+			if(a>b && c<b){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +b;
+				textBox6.Text = "Terceiro " +c;
+				
+					
+					
+			}if(b>a && c>b){
+				
+				textBox4.Text = "Primeiro " +c;
+				textBox5.Text = "Segundo " +b;
+				textBox6.Text = "Terceiro " +a;
+				
+				
+				
+				
+			}if(c>a && b<a){
+				
+				textBox4.Text = "Primeiro " +c;
+				textBox5.Text = "Segundo " +a;
+				textBox6.Text = "Terceiro " +b;
+				
+				
+				
+			}if(b>c && c<a){
+				
+				textBox4.Text = "Primeiro " +b;
+				textBox5.Text = "Segundo " +a;
+				textBox6.Text = "Terceiro " +c;
+				
+				
+				
+			}if(b>c && a<c){
+				
+				textBox4.Text = "Primeiro " +b;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +a;
+				
+				
+			}if(a>b && b<c){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +b;
+				
+				
+				
+			}if(a==b && b>c){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +b;
+				textBox6.Text = "Terceiro " +c;
+				
+			
+			}if(a==c && b<c){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +b;
+				
+		
+			}if(a==b && b<c){
+				
+				textBox4.Text = "Primeiro " +c;
+				textBox5.Text = "Segundo " +b;
+				textBox6.Text = "Terceiro " +a;
+		
+			
+			}if(a==c && b>c){
+				
+				textBox4.Text = "Primeiro " +b;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +a;
+		
+			
+			}if(b==c && a>c){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +b;
+			
+			
+			}if(b==c && a<c){
+				
+				textBox4.Text = "Primeiro " +b;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +a;
+			
+			
+			}if(a==c && b==c){
+				
+				textBox4.Text = "Primeiro " +a;
+				textBox5.Text = "Segundo " +c;
+				textBox6.Text = "Terceiro " +b;
+				
+			}
+			
+			
+			//Correção
+			
+			int maior, meio, menor, n;
+			
+			maior = int.Parse(textBox1.Text);
+			
+			n     = int.Parse(textBox2.Text);
+			
+			if (n > maior)
+			{
+			   meio = maior;
+			   maior = n;
+			}
+			else 
+			{
+			   meio = n;
+			}
+			
+			n = int.Parse(textBox3.Text);
+			
+			if (n > maior)
+			{
+			   menor = meio;
+			   meio = maior;
+			   maior = n;
+			}
+			else if (n > meio)
+			{
+			  menor = meio;
+			  meio = n;
+			}
+			else
+			{
+			  menor = n;
+			}
+			
+			textBox4.Text = "Primeiro " +menor;
+			textBox5.Text = "Segundo " +meio;
+			textBox6.Text = "Terceiro " +maior;
+			
+			
+			
+			//listBox1.Clear();
+			
+			//listBox1.Items.Add(menor);
+			//listBox1.Items.Add(meio);
+			//listBox1.Items.Add(maior);
+		
+			//Correção 2.0
+			
+			maior = int.Parse(textBox1.Text);
+			meio  = int.Parse(textBox2.Text);
+			
+			
+			
+			if (meio > maior)
+			{
+				
+				//Alogitmo de troca (swap)
+				
+				n = meio;
+				meio = maior;
+				maior = n;
+				
+			}	
+			
+			menor = int.Parse(textBox3.Text);
+			
+			if (menor > maior)
+			{
+				
+				n = menor;
+				menor = meio;
+				meio = maior;
+				maior = n;	
+			}
+			else if (menor > meio)
+			{
+				n = menor;
+				menor = meio;
+				meio = n;
+			}
+		
+		}
+	}
+}
