9618143aafb93c4ffe12b8e5ef197fc907a44392
EduardoAndretta <82424514+EduardoAndretta@users.noreply.github.com>
2021-10-15 16:12:12 -0300

    Troca

---
+++ Troca/Troca/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo Andretta
+ * Date: 28/04/2021
+ * Time: 20:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Troca
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			//Declarando e Requisitando as Variáveis
+			
+			int a, b, aux;
+			
+			//Requsitando Dados
+			
+			Console.Write("Valor A:");
+			a = int.Parse(Console.ReadLine());
+			Console.Write("Valor B:");
+			b = int.Parse(Console.ReadLine());
+			
+			//Efetuando a Troca1
+			
+			aux = b;
+			b = a;
+			a = aux;
+				
+			//Exibindo o Resultado
+		
+			Console.WriteLine();
+			Console.WriteLine("A: " +a);
+			Console.WriteLine("B: " +b);
+			Console.WriteLine();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
